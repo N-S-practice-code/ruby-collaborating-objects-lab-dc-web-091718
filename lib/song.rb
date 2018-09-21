cf7ba0652f9824ae427efafe7e6a90f51404c4e7
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-21 20:23:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,9 @@ class Song
   
   
   def self.new_by_filename(filename)
-    
+    song=self.new(filename.split(' - ')[1].split('.')[0])
+    song.artist_name=filename.split(' - ')[0]
+    song
   end
   def self.new_from_filename(filename)
     song=self.new(filename.split(' - ')[1].split('.')[0])
